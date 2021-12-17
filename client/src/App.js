4962e9054777069f05b278de0439bb07b2e581f6
Andrei Rozoleanu <a.rozoleanu@gmail.com>
2021-12-17 11:13:12 +0000

    Create Components Folder and Start Work
    * create "components" folder in client/src
    * add "Author.js", "Book.js" and "Category.js" files for respective components
    * start work on Category component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, {Fragment} from 'react';
 import './App.css';
 
+import Category from './components/Category';
+
 function App() {
   return (
     <Fragment>
-        
+        <Category />
     </Fragment>
   );
 }
---
+++ client/src/components/Category.js
@@ -0,0 +1,32 @@
+import React, { Fragment, useState, useEffect } from 'react';
+
+const Category = () => {
+    const [categories, setCategories] = useState([]);
+
+    const getCategories = async () => {
+        try {
+            const response = await fetch('http://localhost:5000/category');
+            const jsonData = await response.json();
+
+            setCategories(jsonData);
+            console.log(jsonData);
+        } catch (err) {
+            console.error(err.message);
+        }
+    };
+
+    useEffect(() => {
+        getCategories();
+    }, []);
+
+    return <Fragment>                
+        <h1 style={{textAlign: "center"}}>READING LIST</h1>
+        <ul className="mt-5 text-center">
+            {categories.map(category => (
+                <button>{category.description}</button>
+            ))}                
+        </ul>        
+    </Fragment>
+};
+
+export default Category;
